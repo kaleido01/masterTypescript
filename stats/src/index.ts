42e61ff9fb72c6d789bbdfb7d6af9bd149036017
kaleido <kaleido@gmail.com>
2019-06-29 22:54:28 +0900

    genericの実装

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,4 +1,17 @@
-import { CsvFileReader } from "./CsvFileReader";
-const reader = new CsvFileReader("football.csv");
+import { MatchReader } from "./MatchReader";
+import { MatchResult } from "./MatchResult";
+const reader = new MatchReader("football.csv");
+reader.read();
+let manUnitedWins = 0;
 
 console.log(reader);
+
+for (let match of reader.data) {
+	if (match[1] === "Man United" && match[5] === MatchResult.HomeWin) {
+		manUnitedWins++;
+	} else if (match[2] === "Man United" && match[5] === MatchResult.AwayWin) {
+		manUnitedWins++;
+	}
+}
+
+console.log(manUnitedWins);
