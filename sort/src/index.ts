c36ffd19dd83a0a0bd801e6287c8ff88fd1fe6b1
kaleido <kaleido@gmail.com>
2019-06-29 10:24:06 +0900

    abstractの実装

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,11 +1,11 @@
 import { Sorter } from "./Sorter";
 import { CharactersCollection } from "./CharactersCollection";
+import { NumbersCollection } from "./NumbersCollection";
 
-// const numbersCollection = new NumbersCollection([10, 3, -5, 0, 4]);
-// const sorter = new Sorter(numbersCollection);
+const numbersCollection = new NumbersCollection([10, 3, -5, 0, 4]);
+numbersCollection.sort();
+console.log(numbersCollection);
+
+// const charactersCollection = new CharactersCollection("Xaayb");
 // sorter.sort();
-
-const charactersCollection = new CharactersCollection("Xaayb");
-const sorter = new Sorter(charactersCollection);
-sorter.sort();
-console.log(sorter);
+// console.log(sorter);
