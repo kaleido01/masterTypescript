23387e71ccc0abcf602ca47dc1debacd547632a4
kaleido <kaleido@gmail.com>
2019-06-29 09:32:46 +0900

    共通ロジックの切り出し

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,4 +1,8 @@
-const sorter = new Sorter([10, 3, -5, 0]);
+import { Sorter } from "./Sorter";
+import { NumbersCollection } from "./NumbersCollection";
+
+const numbersCollection = new NumbersCollection([10, 3, -5, 0, 4]);
+const sorter = new Sorter(numbersCollection);
 
 sorter.sort();
 console.log(sorter);
