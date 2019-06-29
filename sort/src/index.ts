88adee926cb2e40b84c01bc35e722609a55b9876
kaleido <kaleido@gmail.com>
2019-06-29 10:06:18 +0900

    character sortの実装

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,8 +1,11 @@
 import { Sorter } from "./Sorter";
-import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0, 4]);
-const sorter = new Sorter(numbersCollection);
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0, 4]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
 
+const charactersCollection = new CharactersCollection("Xaayb");
+const sorter = new Sorter(charactersCollection);
 sorter.sort();
 console.log(sorter);
