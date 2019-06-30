60c91da0d70547fe638046b9073025ddc99f3cac
kaleido <kaleido@gmail.com>
2019-06-30 09:48:11 +0900

    set reducer action

--- rrts/src/index.tsx
+++ rrts/src/index.tsx
@@ -1,44 +1,57 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { App } from "./components/App";
+import { reducers } from "./reducers/index";
 
-interface Props {
-	color: string;
-}
+const store = createStore(reducers, applyMiddleware(thunk));
 
-// interface State {
-// 	counter: number;
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+
+	document.getElementById("root")
+);
+// interface Props {
+// 	color: string;
 // }
 
-// const FuncApp = (props: Props): JSX.Element => {
-// 	return <div>{props.color}</div>;
-// };
+// // interface State {
+// // 	counter: number;
+// // }
 
-class App extends React.Component<Props> {
-	state = { counter: 0 };
+// // const FuncApp = (props: Props): JSX.Element => {
+// // 	return <div>{props.color}</div>;
+// // };
 
-	// constructor(props: Props, state: State) {
-	// 	super(props);
-	// 	this.state = {
-	// 		counter: 0
-	// 	};
-	// }
+// class App extends React.Component<Props> {
+// 	state = { counter: 0 };
 
-	onIncrement = (): void => {
-		this.setState({ counter: this.state.counter + 1 });
-	};
-	onDecrement = (): void => {
-		this.setState({ counter: this.state.counter - 1 });
-	};
+// 	// constructor(props: Props, state: State) {
+// 	// 	super(props);
+// 	// 	this.state = {
+// 	// 		counter: 0
+// 	// 	};
+// 	// }
 
-	render() {
-		return (
-			<div>
-				<button onClick={this.onIncrement}>Increment</button>
-				<button>Decrement</button>
-				{this.state.counter}
-			</div>
-		);
-	}
-}
+// 	onIncrement = (): void => {
+// 		this.setState({ counter: this.state.counter + 1 });
+// 	};
+// 	onDecrement = (): void => {
 
-ReactDOM.render(<App color="red" />, document.getElementById("root"));
+// 		this.setState({ counter: this.state.counter - 1 });
+// 	};
+
+// 	render() {
+// 		return (
+// 			<div>
+// 				<button onClick={this.onIncrement}>Increment</button>
+// 				<button>Decrement</button>
+// 				{this.state.counter}
+// 			</div>
+// 		);
+// 	}
+// }
